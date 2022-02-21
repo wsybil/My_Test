9cf80f363959f5421b88dccbeef37bb43b9639c2
WSybil <wsybil@student.21-school.ru>
2022-02-21 23:10:39 +0300

    Update@

--- task1/task1.py
+++ task1/task1.py
@@ -7,7 +7,8 @@ def main():
     try:
         n = sys.argv[1]
         m = sys.argv[2]
-        
+        lst = [n for n in range(int(n))][1:]
+        print(lst)
     except IndexError:
         print("Error input")
 
