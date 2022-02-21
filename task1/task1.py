ef8ee2eab3a78d868003889eea82d6f40bcb8de2
WSybil <wsybil@student.21-school.ru>
2022-02-21 22:38:36 +0300

    Update

--- task1/task1.py
+++ task1/task1.py
@@ -1,1 +1,15 @@
-print("Hello world")
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import sys
+
+def main():
+    try:
+        n = sys.argv[1]
+        m = sys.argv[2]
+        
+    except IndexError:
+        print("Error input")
+
+if __name__ == "__main__":
+    main()
