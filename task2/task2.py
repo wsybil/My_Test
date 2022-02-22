7e7481067a3f26b9e80323d7e5516a18aba3dae6
WSybil <wsybil@student.21-school.ru>
2022-02-22 21:08:46 +0300

    task2

--- task2/task2.py
+++ task2/task2.py
@@ -2,14 +2,9 @@
 # -*- coding: UTF-8 -*-
 
 import sys
-import math
 
 
 def main():
-    x = float()
-    y = float()
-    r = float()
-
     try:
         if len(sys.argv) > 3:
             print("Error input")
@@ -17,24 +12,26 @@ def main():
 
         file1 = sys.argv[1]
         with open(file1, 'r') as f1:
-            for line in f1:
-                print(line)
+            text = f1.read()
+
+        text_nums = text.split()
+        c_x, c_y, r = float(text_nums[0]), float(text_nums[1]), float(text_nums[2])
 
         file2 = sys.argv[2]
         with open(file2, 'r') as f2:
             for line in f2:
-                print(line)
+                coord = line.split()
+                x, y = float(coord[0]), float(coord[1])
+                h = (x - c_x) ** 2 + (y - c_y) ** 2
+                if str(h) == str(r ** 2):
+                    print(0)
+                elif h < r ** 2:
+                    print(1)
+                else:
+                    print(2)
 
     except IndexError:
         print("Error input")
-
-    h = math.sqrt(x ** 2 + y ** 2)
-    if h == r:
-        print(0) # лежит
-    elif h < r:
-        print(1) # внутри
-    else:
-        print(2) # снаружи
 
 
 if __name__ == "__main__":
