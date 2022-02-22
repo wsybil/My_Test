adf793574d692d72395799ad8e9c723fee1d9c48
WSybil <wsybil@student.21-school.ru>
2022-02-22 10:16:00 +0300

    task1

--- task1/task1.py
+++ task1/task1.py
@@ -3,14 +3,29 @@
 
 import sys
 
+
 def main():
     try:
         n = sys.argv[1]
         m = sys.argv[2]
-        lst = [n for n in range(int(n))][1:]
-        print(lst)
+        lst = int(m) * [int(i) for i in range(1, int(n) + 1)]
+        lst2 = [' ']
+        lst3 = []
+        count = 0
+        while lst2[-1] != 1:
+            lst2.clear()
+            for j in range(count, int(m) + count):
+                lst2.append(lst[j])
+                count += 1
+            lst2c = lst2.copy()
+            lst3.append(lst2c)
+            count -= 1
+        for k in range(len(lst3)):
+            print(lst3[k][0], end='')
+
     except IndexError:
         print("Error input")
+
 
 if __name__ == "__main__":
     main()
